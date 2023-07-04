05247bf4723248c3f6f76a8d5111b9887e2436db
Mackenzie James <mackenzie.14@hotmail.co.uk>
2023-07-04 19:03:41 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Busa\Seat\Http\Controllers\ApiController;
 
-use Busa\Seat\Http\Controllers;
+use Busa\Seat\Http\Controllers\BUSAController;
 
-Route::group([
-    'namespace' => 'Busa\Seat\Http\Controllers',
-    'prefix' => 'api',
-], function () {
-    Route::get('/', [ApiController::class, 'index']);
-    Route::get('/busa', [BUSAController::class, 'busa'])->name('busa.dashboard');
-});
+Route::get('/busa', [BUSAController::class, 'busa'])->name('busa.dashboard');
